928b3c17cd7bc77cbfb86d16343b51f60885a076
Yousuf-Mohammad <yousuf.mohammad8783@gmail.com>
2025-11-04 00:09:08 +0600

    Update Next.js configuration to enable static export and optimize image handling by setting unoptimized flag. Add new script for serving static preview.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   outputFileTracingRoot: __dirname,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
